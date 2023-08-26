a9cfb61c7f3d5a4cb6c64c8ec5766a0c5c681cb7
ssoni <snsoni.work@gmail.com>
2023-08-26 07:11:13 -0500

    GUI is created

---
+++ IData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmgPrsrDropCal
+{
+    internal interface IData
+    {
+        double inletPrsr { get; set; }
+        double inletOilRate { get; set; }
+        double inletWaterRate { get; set; }
+        double inletGasRate { get; set; }
+        double pipeId { get; set; }
+        double pipeLength { get; set; }
+        double incliantion { get; set; }
+        double oilVis { get; set; }
+        double gasVis { get; set; }
+        double waterVis { get; set; }
+    }
+}
---
+++ PrsrDrop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CmgPrsrDropCal
+{
+    public class PrsrDrop
+    {
+
+
+        public PrsrDrop()
+        {
+
+        }
+
+        public double inletPrsr { get; set; }
+        public double inletOilRate { get; set; }
+        public double inletWaterRate { get; set; }
+        public double inletGasRate { get; set; }
+        public double pipeId { get; set; }
+        public double pipeLength { get; set; }
+        public double incliantion { get; set; }
+        public double oilVis { get; set; }
+        public double gasVis { get; set; }
+        public double waterVis { get; set; }
+
+    }
+
+   
+
+
+}
